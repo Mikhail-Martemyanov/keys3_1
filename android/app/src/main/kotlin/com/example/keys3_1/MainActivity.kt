29e577bf909dc23a5dae71aa1f43b1c687113dab
mihn23 <martemyanov.mv@mail.ru>
2022-03-10 22:35:14 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/keys3_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.keys3_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:keys3_1/keys31.dart';
+import 'dart:async';
+import 'dart:io';
+
+void main() {
+  runApp(const MyApp());
+}
